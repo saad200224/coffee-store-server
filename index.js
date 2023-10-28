05c76c865388a014c975e6f6fe3b244c0eb4bbe4
saad200224 <saad19021994@outlook.com>
2023-10-28 23:42:22 +0600

    first

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.use(express.json());
 // Username: saadibnesaifullah051
 // Pass: 1ZC7E0UDLeILPhuK
 
+// DB_USER=saadibnesaifullah051
+// DB_PASS=1ZC7E0UDLeILPhuK
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xgkcrfh.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -53,6 +55,28 @@ async function run() {
       console.log(newCoffee)
       const result = await coffeeCollection.insertOne(newCoffee);
       res.send(result);
+    })
+
+    app.put("/coffee/:id", async (req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true }
+      const updatedCoffee = req.body
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name,
+          quantity: updatedCoffee.quantity,
+          supplier: updatedCoffee.supplier,
+          taste: updatedCoffee.taste,
+          category: updatedCoffee.category,
+          details: updatedCoffee.details,
+          photo: updatedCoffee.photo
+        }
+      }
+
+      const result = await coffeeCollection.updateOne(filter, coffee, options);
+      res.send(result);
+
     })
 
     app.delete("/coffee/:id", async(req, res)=>{
